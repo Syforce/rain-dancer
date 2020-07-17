eef687fd21afa390069d2c0290486738dc48ba07
unknown <malina.zgavirdici99@yahoo.com>
2020-07-17 14:16:39 +0300

    added: listing/listing cropped image and profile/profile cropped image for Talent

--- src/app/talent/talent/talent.component.ts
+++ src/app/talent/talent/talent.component.ts
@@ -16,9 +16,13 @@ import { FormFile } from '@shared/service/model/form-file.model';
 })
 export class TalentComponent extends AbstractComponent<Talent> {
 	private mediaService: MediaService;
-	private file;
+	private listingFile: File;
+	private profileFile: File;
 
-	public originalImage: any = '';
+	public listingImage: any = '';
+	public profileImage: any = '';
+	public listingCroppedImage: any = '';
+	public profileCroppedImage: any = '';
 	public medias: Array<Media> = new Array<Media>();
 
 	constructor(service: TalentService, activatedRoute: ActivatedRoute, router: Router, mediaService: MediaService) {
@@ -38,24 +42,70 @@ export class TalentComponent extends AbstractComponent<Talent> {
 	}
 
 	public onFileChange(event) {
-		const reader = new FileReader();
-
-		this.file = event.target.files[0];
-		reader.readAsDataURL(event.target.files[0]);
-		reader.onload = () => {
-			this.originalImage = reader.result;
+		if (event.target.name === 'listingImage') {
+			this.listingImage = event;
+			this.listingFile = event.target.files[0];
+		} else {
+			this.profileImage = event;
+			this.profileFile = event.target.files[0];
 		}
 	}
 
-	public save() {
-		const image: FormFile = {
-			file: this.file,
-			key: 'storeImage'
-		};
+	public onListingCropped(event) {
+		this.listingCroppedImage = event.base64;
+	}
 
-		this.service.createForm(this.item, [image]).subscribe((data) => {
-			console.log(data);
+	public onProfileCropped(event) {
+		this.profileCroppedImage = event.base64;
+	}
+
+	public save() {
+		fetch(this.profileCroppedImage)
+		.then(result => result.blob())
+		.then((blob) => {
+			const file: File = new File([blob], 'profileCropped', {
+				type: 'image/jpeg'
+			});
+
+			const profile: FormFile = {
+				file: this.profileFile,
+				key: 'profileImage'
+			};
+
+			const profileCropped: FormFile = {
+				file: file,
+				key: 'profileCroppedImage'
+			};
+
+			console.log(profile);
+			console.log(profileCropped);
+
+			fetch(this.listingCroppedImage)
+			.then(result => result.blob())
+			.then((blob) => {
+				const file: File = new File([blob], 'listingCropped', {
+					type: 'image/jpeg'
+				});
+	
+				const listing: FormFile = {
+					file: this.listingFile,
+					key: 'listingImage'
+				};
+	
+				const listingCropped: FormFile = {
+					file: file,
+					key: 'listingCroppedImage'
+				};
+
+				console.log(listing);
+				console.log(listingCropped);
+	
+				this.service.createForm(this.item, [profile, profileCropped, listing, listingCropped]).subscribe((data) => {
+					console.log(data);
+				});
+			});
 		});
+		
 	}
 
 	public update() {
